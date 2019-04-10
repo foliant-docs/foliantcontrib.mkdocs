12f3c3a4eadef904211a107feff65c088e54bce5
Artemy Lomov <lomov@restream.rt.ru>
2019-04-10 18:40:58 +0300

    Bump version to 1.0.8.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description=SHORT_DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
-    version='1.0.7',
+    version='1.0.8',
     author='Konstantin Molchanov',
     author_email='moigagoo@live.com',
     url='https://github.com/foliant-docs/foliantcontrib.mkdocs',
